160b09213db13898565247c960df3aef7e9bffa7
Hitesh Rijhwani <rijhwanihitesh23@gmail.com>
2019-12-10 21:25:57 -0600

    Singly

---
+++ src/com/linked/list/SinglyLinkedListNode.java
@@ -0,0 +1,17 @@
+package com.linked.list;
+
+class Node{
+    Node next;
+    int val;
+    public Node(int val){
+        this.val=val;
+    }
+
+}
+
+public class SinglyLinkedListNode {
+
+
+
+
+}
