24817179804b5b7edc9f0743528a5491c70e51b8
Hitesh Rijhwani <rijhwanihitesh23@gmail.com>
2020-03-01 16:32:25 -0600

    singleton

---
+++ src/com/singleton/SingletonEnum.java
@@ -0,0 +1,19 @@
+package com.singleton;
+
+public enum SingletonEnum {
+    /**
+     * Implementing Singleton in Java 5 or above version using Enum:
+     * Enum is thread safe and implementation of Singleton through Enum ensures that your singleton will have only one instance even
+     * in a multithreaded environment. Let us see a simple implementation:
+     */
+    INSTANCE;
+
+    public void doSomething(){
+        System.out.println(INSTANCE.equals(INSTANCE));
+    }
+
+    public static void main(String [] args){
+        SingletonEnum.INSTANCE.doSomething();
+        SingletonEnum.INSTANCE.doSomething();
+    }
+}
